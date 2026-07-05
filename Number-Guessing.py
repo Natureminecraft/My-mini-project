4f58b12ef5eaf5c3decd24d871e9dc351a0b62ab
Nature <sotormach@gmail.com>
2026-07-05 15:09:38 +0700

    Add files via upload

---
+++ Number-Guessing.py
@@ -0,0 +1,45 @@
+import random
+
+def askmax():
+    try:
+        maxnum = abs(int(input("What is the highest number you want? : ")))
+        return(maxnum)
+    except:
+        print("Please enter a valid number.")
+        return(askmax())
+
+def ask(value):
+    try:
+        ans = int(input("Guess the number between 1-"+str(value)+": "))
+        return(ans)
+    except:
+        print("Please enter a valid number.")
+        return(ask(value))
+
+def check(innum, num, maxnum):
+    if innum == num:
+        print("Correct")
+        print("-----------------------------------------------")
+        return("break")
+    elif innum > maxnum or innum < 1:
+        print("The number is between 1-"+str(maxnum))
+        print()
+    elif innum > num:
+        print("Lower")
+        print()
+    elif innum < num:
+        print("Higher")
+        print()
+
+maxnum = int()
+
+while True:
+    maxnum = askmax()
+    if maxnum:
+        break
+
+num = random.randint(1, maxnum)
+
+while True:
+    if check(ask(maxnum), num, maxnum) == "break":
+        break
